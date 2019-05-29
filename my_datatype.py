d597649663ca97634c5e83ac0c441c72c0eb2734
udiedric <32105982+udiedric@users.noreply.github.com>
2019-05-29 09:10:44 +0200

    Add files via upload

---
+++ my_datatype.py
@@ -0,0 +1,7 @@
+list = ["Apple", "Orange", "Banana"]
+tuple = ("Apple", 10, "Orange", 20, "Banana", 30)
+dic = {"Apple":"green", "Orange":"Spain", "Banana":"yellow"}
+
+print (list)
+print (tuple)
+print (dic)
